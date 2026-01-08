10c2822f3169db1476f5f3bb2a57e20b6f3edd96
sttech321 <st.tech321@gmail.com>
2026-01-08 22:58:08 +0530

    latest change for server.js

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/emails', routes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on port ${PORT}`);
 });
